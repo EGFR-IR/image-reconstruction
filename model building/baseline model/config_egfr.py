8c78d50c16089ec899d1eeb2ff31a645a73358b4
EGFR-Prediction <137248473+EGFR-Prediction@users.noreply.github.com>
2023-07-01 19:30:20 +0800

    Update config_egfr.py

--- model building/baseline model/config_egfr.py
+++ model building/baseline model/config_egfr.py
@@ -1,27 +1,25 @@
-# -*- coding: utf-8 -*
 import warnings
 import torch as t
 
 
 class DefaultConfig(object):
-    env = 'gdq-main'  # visdom 环境
-    vis_port = 8097   # visdom 端口
-    model = 'MDenseNet'  # 使用的模型，名字必须与models/__init__.py中的名字一致MDenseNet resnet50_1228
+    env = 'gdq-main'  
+    vis_port = 8097  
+    model = 'MDenseNet' 
     ex_num = '001+[1,0.6]'
 
-    train_data_root = '/media/diskF/lar/data/split_600/npy_crop_600/image/'  # 训练集存放路径
-    train_data_label = '/media/diskF/lar/data/split_600/npy_crop_600/mask/'
+    train_data_root = '/media/diskF/data/split_600/npy_crop_600/image/'  
+    train_data_label = '/media/diskF/data/split_600/npy_crop_600/mask/'
 
-    val_data_root = train_data_root  # 内部验证集存放路径
+    val_data_root = train_data_root 
     val_data_label = train_data_label
-    exval_data_root = '/media/diskF/lar/data/data_95/image/'  # 外部验证集存放路径
+    exval_data_root = '/media/diskF/lar/data/data_95/image/' 
     exval_data_label = '/media/diskF/lar/data/data_95/mask/'
-    test_data_root = train_data_root  # 测试集存放路径
+    test_data_root = train_data_root  
     test_data_label = train_data_label
 
 
-    # 加载预训练的模型的路径，为None代表不加载
-    load_model_path = None
+   load_model_path = None
 
     batch_size = 16  # batch size
     use_gpu = True  # user GPU or not
@@ -37,9 +35,6 @@ class DefaultConfig(object):
     device_ids = [0,1]
 
     def _parse(self, kwargs):
-        """
-        根据字典kwargs 更新 config参数
-        """
         for k, v in kwargs.items():
             if not hasattr(self, k):
                 warnings.warn("Warning: opt has not attribut %s" % k)
